faff4ca1c12f4d4d85bb5ba180e95788c5d531f3
Tyler Harbin-Giuntoli <harbin.giuntoli@gmail.com>
2026-09-23 20:04:54 -0700

    security: use durable expiring OAuth state in hosted app

--- src/personal_algorithm/application.py
+++ src/personal_algorithm/application.py
@@ -12,7 +12,8 @@ from .azure_credentials import AzureKeyVaultCredentialStore
 from .bootstrap import BootstrapCoordinator
 from .connections import ConnectionStore
 from .connections_api import create_connections_router
-from .github_auth import AuthorizationStateStore, GitHubAuthorization
+from .github_auth import GitHubAuthorization
+from .oauth_state import DurableAuthorizationStateStore
 from .github_provider import GitHubProvider
 from .instance_auth import InstanceAuth
 from .private_api import protect
@@ -66,7 +67,7 @@ def create_private_app(
         client_id=settings.github_client_id,
         client_secret=settings.github_client_secret,
         credentials=credential_store,
-        states=AuthorizationStateStore(),
+        states=DurableAuthorizationStateStore(store),
         http=oauth_http,
         provider=github,
     )
